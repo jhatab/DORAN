14a175c099fc372323eeaea2a9fb941efcc1ace6
jhatab <jhatab@naver.com>
2022-02-18 17:08:28 +0900

    header

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-    <div className="header">
-      <div className="header_bar">
-      <div className="logo">
-        <p>logo</p>
-      </div>
-      <div className="search_bar">   
-      <p>search</p>     
-      </div>
-      <div className="menu">
-        <p>menu</p>
-      </div>
-      </div>
-    </div>
-    <div className="body">
+      <div className="header">
+        <div className="header_bar">
+          <div className="logo">
+            <a href="#"><img className="logo_img" src="images/logo.png" /></a>
+          </div>
+          <div className="search_bar">
+            <img className="search_icon" src="images/search.png"/>
+              <input className="search_box" type="text" placeholder="Keyword Search" />
+          </div>
+          <div className="menu">
+            <ul className="menu_btn">
+              <li ><img className="alarm_btn" src="images/bell.png" /></li>
+              <a href="#"><li className="login_btn">로그인</li></a>
+            </ul>
+          </div>
+        </div>
     </div>
     </div>
   );
